bfe8d4adb54bd1e472ee090d96367f15dd1d546a
Augusto <agomezc6@miumg.edu.gt>
2020-04-18 17:13:57 -0600

    Inicio de session

--- vista/modulos/navBarraLateral.php
+++ vista/modulos/navBarraLateral.php
@@ -2,19 +2,9 @@
                 <div class="navbar-default sidebar" role="navigation">
                     <div class="sidebar-nav navbar-collapse">
                         <ul class="nav in" id="side-menu">
-                            <li class="sidebar-search">
-                                <div class="input-group custom-search-form">
-                                    <input type="text" class="form-control" placeholder="Search...">
-                                    <span class="input-group-btn">
-                                        <button class="btn btn-primary" type="button">
-                                            <i class="fa fa-search"></i>
-                                        </button>
-                                </span>
-                                </div>
-                                <!-- /input-group -->
-                            </li>
+
                             <li>
-                                <a href="controlDeMesas" class="active"><i class="fa fa-dashboard fa-fw"></i> Control de Mesas</a>
+                                <a href="controlDeMesas" class="active"><i class="fa fa-dashboard fa-fw"></i> DashBoard</a>
                             </li>
                             <li>
                                 <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
