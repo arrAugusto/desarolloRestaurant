cfd638c3616e78e85c8041966688b9e5840b0d44
Augusto <agomezc6@miumg.edu.gt>
2020-04-27 23:20:17 -0600

    cambios en usuarios gestion de edicion, creacion, inactivar y activar usuarios

--- controlador/usuarios.controlador.php
+++ controlador/usuarios.controlador.php
@@ -6,21 +6,20 @@ class ControladorUsuarios {
         if (isset($_POST["ingUsuario"])) {
 
             if (preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingUsuario"])) {
-                $encriptar = crypt($_POST["ingPassword"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+                $passFront = crypt($_POST["ingPassword"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
                 $usuarioFront = $_POST["ingUsuario"];
-                $passFront = $_POST["ingPassword"];
-                var_dump($usuarioFront);
-                var_dump($passFront);
-                
-                $respuesta = ModeloUsuarios::mdlMostrarUsuarios($usuarioFront, $passFront);
-                echo '<br/>';
-                var_dump($respuesta);   
+                $respuesta = ModeloUsuarios::mdlMostrarUsuarios($usuarioFront);
+                var_dump($respuesta);
+                return true;
                 if ($respuesta["usuario"]==$usuarioFront && $respuesta["password"]==$passFront) {
                                             $_SESSION["iniciar"] = "Ok";
                 }
-                
             }
         }
     }
 
+    public static function ctrMostrarUsuarios(){
+       $usuarios = ModeloUsuarios::mdlMostrarUsuariosIndiv();
+       return $usuarios; 
+    } 
 }
